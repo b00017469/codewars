d679ae386a7e46c44549307b6650f0d8c699bd95
Pavel Morozuk <morozuki@mail.ru>
2022-03-06 23:00:34 +0300

    function-persistence

---
+++ function-persistence.js
@@ -0,0 +1,22 @@
+function persistence(num) {
+   let result = 0;
+   while (num > 9) {
+      let arrayNams = String(num).split('');
+      let j = 0;
+      for (let i = 0; i < arrayNams.length - 1; i++) {
+         if (j == 0) {
+            num = arrayNams[i] * arrayNams[i + 1];
+         } else {
+            num *= arrayNams[i + 1];
+         }
+         console.log(num);
+         j++;
+      }
+      result++
+   }
+
+   console.log(result);
+   return result;
+}
+
+persistence(39);
